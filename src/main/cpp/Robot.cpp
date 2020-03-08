8fbf5cb60886e421e54f66afff5868260ef58329
tomuel99 <tomtomhiggs@gmail.com>
2020-03-07 21:24:48 -0700

    Truly a masterpiece

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -23,9 +23,7 @@
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc/SmartDashboard/SendableChooser.h>
 #include <frc/Servo.h>
-//#include <ctre/Phoenix.h>
 #include <PigeonIMU.h>
-//#include <ctre/ctre.h>
 
 #include "rev/SparkMax.h"
 #include <frc/Compressor.h>
